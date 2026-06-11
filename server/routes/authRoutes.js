1d0f348b959af59cce98f02159e8a25644dfb1bd
Vishva Modh <vishvamodh45@gmail.com>
2026-06-11 22:12:58 +0530

    added user login using jwt

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -12,3 +12,26 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 module.exports = router;
+
+//JWT_SECRET is a  guard’s private stamp/signature key
+//JWT token  is a student’s entry pass / ID card
+//User       is a actual student
+// workflow
+/*
+Login:
+email + password
+↓
+backend checks password
+↓
+backend creates token using JWT_SECRET
+↓
+frontend stores token
+
+
+Protected route:
+frontend sends token
+↓
+backend verifies token using JWT_SECRET
+↓
+backend allows/denies request
+*/
