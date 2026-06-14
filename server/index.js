58b1cac7f8cd042280576a0a01d4bb7b1ceb325e
Vishva Modh <vishvamodh45@gmail.com>
2026-06-14 14:55:10 +0530

    deployyy

--- server/index.js
+++ server/index.js
@@ -18,8 +18,24 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  process.env.CLIENT_URL,
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("SportsSphere API is running");
